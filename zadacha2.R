89a3f66cdaade35f6c70b526790ee065a3496aa1
TsogooJuteh <ttsogtoi@gmail.com>
2023-03-13 21:15:36 +0300

    Update zadacha2.R

--- zadacha2.R
+++ zadacha2.R
@@ -3,50 +3,29 @@ library("GGAlly")
 
 data = attitude
 
-#1
+#1 Checking that there is no linear dependence using R^2
 model1 <- lm(rating ~ privileges, data=data)
-model2 <- lm(rating ~ learning, data=data)
+model2 <- lm(rating ~ learning, data=data)#this R^2 is bigger, so it can be excluded
 model3 <- lm(rating ~ raises, data=data)
 
 summary(model1)$r.squared
 summary(model2)$r.squared
 summary(model3)$r.squared
 
-#2
+#2 Checking how good the model is
 model4 <- lm(rating ~ privileges+learning+raises, data=data)
 
 summary(model4)$r.squared
 summary(model4)
 
-#3
+#3 enter the log of the regressors into the model
 model5 <- lm(rating ~ log(privileges)+log(learning)+log(raises), data=data)
 
-summary(model4)$r.squared
+summary(model4)$r.squared#checking which model is good
 summary(model5)$r.squared
 
-#4
-summary(model1)$r.squared#rating ~ privileges
-summary(model2)$r.squared#rating ~ learning
-summary(model3)$r.squared#rating ~ raises
-summary(model4)$r.squared#rating ~ privileges+learning+raises
-
-model6 <- lm(rating ~ privileges+learning, data = data)
-summary(model6)$r.squared
-model7 <- lm(rating ~ privileges+raises, data = data)
-summary(model7)$r.squared
-model8 <- lm(rating ~ learning+raises, data = data)
-summary(model8)$r.squared
-model9 <- lm(rating ~ privileges^2, data = data)
-summary(model9)$r.squared
-model10 <- lm(rating ~ learning^2, data = data)
-summary(model10)$r.squared
-model11 <- lm(rating ~ raises^2, data = data)
-summary(model11)$r.squared
-model12 <- lm(rating ~ privileges^2+learning^2, data = data)
-summary(model12)$r.squared
-model13 <- lm(rating ~ privileges^2+raises^2, data = data)
-summary(model13)$r.squared
-model14 <- lm(rating ~ raises^2+learning^2, data = data)
-summary(model14)$r.squared
-model15 <- lm(rating ~ privileges^2+learning^2+raises^2, data = data)
-summary(model15)$r.squared
+#4 All possible products of pairs of regressors(including squares)
+model6 <- lm(rating ~ privileges+learning+raises+I(privileges^2)+I(learning^2)+I(raises^2), data = data)
+summary(model6)$r.squared#This model is good
+summary(model5)$r.squared
+summary(model4)$r.squared
